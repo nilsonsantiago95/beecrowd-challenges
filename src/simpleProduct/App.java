ff32f5c61e4c4d98ebb688737c8b926aaebe0c08
Nilson Santiago <nilson.santiago19@gmail.com>
2025-01-28 22:54:59 -0300

    Simple prod

---
+++ src/simpleProduct/App.java
@@ -0,0 +1,22 @@
+package simpleProduct;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int prod = a * b;
+
+        System.out.println("PROD = " + prod);
+
+        sc.close();
+
+    }
+
+}
